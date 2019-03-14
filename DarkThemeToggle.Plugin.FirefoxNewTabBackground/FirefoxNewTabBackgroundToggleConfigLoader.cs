a3d4e5bf971d2972f8dfe7677e8f7f1a723c5462
Vladislav Prekel <misterptits@yandex.ru>
2019-03-14 14:40:10 +0700

    FirefoxNewTabBackgroundToggle

---
+++ DarkThemeToggle.Plugin.FirefoxNewTabBackground/FirefoxNewTabBackgroundToggleConfigLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using DarkThemeToggle.Core;
+
+namespace DarkThemeToggle.Plugin.FirefoxNewTabBackground
+{
+	public class FirefoxNewTabBackgroundToggleConfigLoader : AbstractPluginConfigLoader<FirefoxNewTabBackgroundToggleConfigLoader.Config>
+	{
+		public class Config
+		{
+			public string PathTo_userChrome_css { get; } =
+				"C:\\Users\\vladislav\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\tiepgc3k.default-nightly\\chrome\\userChrome.css";
+			public string PathTo_userContent_css { get; } =
+				"C:\\Users\\vladislav\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles\\tiepgc3k.default-nightly\\chrome\\userContent.css";
+
+			public string DarkColorHex { get; } = "#202120";
+			public string LightColorHex { get; } = "#ffffff";
+
+			public string userChrome_css { get; } = @"@-moz-document url(""about:newtab"") {
+body { background-color: {{COLOR}} !important;}
+}
+
+browser { background-color: {{COLOR}} !important; }";
+
+			public string userContent_css { get; } = @"@-moz-document url(""about:newtab"") {
+    body { background-color: {{COLOR}} !important;}
+}
+    
+@-moz-document url(chrome://browser/content/browser.xul) {
+    browser[type=""content-primary""] {background: {{COLOR}} !important}
+}";
+
+			public string ReplaceString { get; } = "{{COLOR}}";
+
+		}
+
+		public override string ConfigFileName { get; } = "DarkThemeToggle.Plugin.FirefoxNewTabBackground.json";
+	}
+}
